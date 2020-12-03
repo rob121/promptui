fd464b9c8bbb33017b51192fd4c9c7f854a27052
rob121 <rob121@gmail.com>
2020-12-02 21:08:27 -0500

    Update select.go

--- select.go
+++ select.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/chzyer/readline"
 	"github.com/juju/ansiterm"
-	"github.com/manifoldco/promptui/list"
-	"github.com/manifoldco/promptui/screenbuf"
+	"github.com/rob121/promptui/list"
+	"github.com/rob121/promptui/screenbuf"
 )
 
 // SelectedAdd is used internally inside SelectWithAdd when the add option is selected in select mode.
